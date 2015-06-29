f76893025c1fdc6c0519d875f881bc066def90fc
Juston Points <justonpoints@gmail.com>
2015-06-28 23:52:44 -0700

    initial

---
+++ README.md
@@ -0,0 +1,30 @@
+# koa-bookshelf-api
+Koa Bookshelf passport starter files
+
+install : node, postrges(create a database)
+
+1. git clone https://github.com/justonpoints/koa-bookshelf-api
+2. npm install (in the root of the app)
+3. Add Postgres creds to index.js knex connection
+4. Update table name in lib/api/api.js
+5. node --harmony index.js (in the root of the app, --harmony required since we are using generators)
+
+5. Login to the app
+http://localhost:1337/api/login?username=foo&password=bar 
+
+6. Make a sample query
+Create : http://localhost:1337/api/test/ (Post)
+SelectById : http://localhost:1337/api/test/{id} (GET)
+Update : http://localhost:1337/api/test/{id} Postdata:[data to update](PUT)
+DELETE : http://localhost:1337/api/test/{id} (DELETE)
+
+
+Plans: 
+
+1. To flush this out further.
+2. Add Migration.
+3. Update the Authentication.
+4. Add testing for the api.
+5. Continue to explore the organzational theory being approached here.
+	- All dependencies declared in the index.js. Dependencies are not declared in the lib files.
+	- All middleware components refrenced in the /lib/lib.js file. Each instaces takes at least an "app" depedency for the koa app.
---
+++ index.js
@@ -0,0 +1,73 @@
+//node libraries, should also be contained in the /package.json file
+//Philosophy, load all depedencies at initialization.
+var koa = require('koa');
+var koa_router = require('koa-router');
+var body = require('koa-body')();
+var request = require('koa-request');
+var session = require('koa-generic-session');
+var mount = require('koa-mount');
+var logger = require('koa-logger');
+var limit = require('koa-better-ratelimit');
+var pg = require('co-pg')(require('pg'));
+
+var knex = require('knex')({
+  client: 'postgresql',
+  connection: {
+    host     : 'localhost',
+    port     : '5432',
+    user     : 'postgres',
+    password : 'postgres',
+    database : 'node',
+    charset  : 'UTF8_GENERAL_CI'
+  }//move this to a config file.
+});
+var Bookshelf = require('bookshelf')(knex);
+
+var passport = require('koa-passport');
+var local = require('passport-local').Strategy;
+
+//local libraries
+var nodes = require('./lib/nodes.js');
+var bookshelf = require('./lib/bookshelf.js')(Bookshelf);
+var auth = require('./lib/auth')(passport,local);//authenticator
+var api = require('./lib/api/api.js')(bookshelf);//main api, requires an authenticated session
+var public_api = require('./lib/api/public_api.js')(auth);//api that can accessed without an active session.
+
+//initializers
+var app = koa();
+var router = new koa_router(); //this is for the authenticated api
+var public_router = new koa_router();//for the non-authenicated api.
+
+
+//__________________________________________________________________ 
+//The component order is important.
+//Each of the middleware component are organized into nodes that require the koa "app".
+//They are called nodes, because of what they characterize. Each node represents a set of processes, and may include for than one app.use
+// down flow (node1) => (node2) => ... (node x), upflow (node x) => .. => (node 1).
+// Each Node can continue the flow, Split the flow*, or terminate the downflow. When the downflow if terminate, that the process works back up.
+//__________________________________________________________________ 
+
+//Error handler
+nodes.error(app);
+
+//Logger, write errors to the console
+nodes.logger(app,logger,true);
+
+//limiter
+nodes.limiter(app,limit);
+
+//session and authentication handler
+nodes.session(app,session,auth);
+
+//routes accessible without a session. This includes login and log out.
+nodes.public_routes(app,public_router,mount,body,public_api);
+
+//The following routes require a valid session.
+nodes.routes(app,router,mount,body,api);
+
+if (!module.parent) app.listen(1337);//move the port to a config file.
+console.log('the Api is running on port 1337');
+//__________________________________________________________________
+
+
+  
