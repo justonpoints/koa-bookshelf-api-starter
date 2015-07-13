92122564881365f94583cd901ff87ef40dab5f48
Juston Points <justonpoints@gmail.com>
2015-07-12 22:54:55 -0700

    added knex db connector

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,10 @@
 // Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: {
-      database: 'node',
+      database: 'test',
       user:     'postgres',
       password: 'postgres'
     },
