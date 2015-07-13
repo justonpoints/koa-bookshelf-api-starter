27a8a327c7dcd4478e40bd223e03b60f879ca2fc
Juston Points <justonpoints@gmail.com>
2015-07-12 22:55:54 -0700

    reorganized and readjusted naming to fit with updates

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //Philosophy, load all depedencies at initialization.
 
-//------------------------------------------------------------------------------------------------------------------------
+//enviroment
+var db_env = require('./knexfile.js').development
+
+
 //node libraries, should also be contained in the /package.json file
+//------------------------------------------------------------------------------------------------------------------------
 var koa = require('koa');
 var koa_router = require('koa-router');
 var body = require('koa-body')();
@@ -11,35 +15,44 @@ var mount = require('koa-mount');
 var logger = require('koa-logger');
 var limit = require('koa-better-ratelimit');
 var pg = require('co-pg')(require('pg'));
-var knex = require('knex')({
-  client: 'postgresql',
-  connection: {
-    host     : 'localhost',
-    port     : '5432',
-    user     : 'postgres',
-    password : 'postgres',
-    database : 'node',
-    charset  : 'UTF8_GENERAL_CI'
-  }//move this to a config file.
-});
+var knex = require('knex')(db_env);
 var Bookshelf = require('bookshelf')(knex);
+var checkit = require("checkit");
 var passport = require('koa-passport');
 var local = require('passport-local').Strategy;
+//------------------------------------------------------------------------------------------------------------------------
+
 
 //local libraries
-var nodes = require('./lib/nodes.js');
-var bookshelf = require('./lib/bookshelf.js')(Bookshelf);
-var auth = require('./lib/auth')(passport,local);//authenticator
-var api = require('./lib/api/api.js')(bookshelf);//main api, requires an authenticated session
+//------------------------------------------------------------------------------------------------------------------------
+//These are sets of app.use statments. They are the primary building blocks of the app.
+var nodes = require('./lib/nodes/nodes.js');
+
+//The collection of database models/books.
+var bookshelf = require('./lib/books/bookshelf.js')(Bookshelf,checkit);
+
+//The collection of queries/pages
+var books = require('./lib/pages/books.js')(bookshelf);
+
+//Autentication setup
+var auth = require('./lib/routes/auth')(passport,local,books.auth);//authenticator
+
+//The main api for the app. Should not be qaccessed unless the session is authenticated.
+var api = require('./lib/api/api.js')(books);//main api, requires an authenticated session
+
+//The api that is universally accessible. 
 var public_api = require('./lib/api/public_api.js')(auth);//api that can accessed without an active session.
+//------------------------------------------------------------------------------------------------------------------------
 
 //initializers
-var app = koa();
+var app = koa();//Start Koa
 var router = new koa_router(); //this is for the authenticated api
-require('./lib/routes.js')(router, body, api);//intialize routes
+require('./lib/routes/routes.js')(router, body, api);//intializes routes
 var public_router = new koa_router();//for the non-authenicated api.
-require('./lib/public_routes.js')(public_router, body, public_api);//intialize public routes
+require('./lib/routes/public_routes.js')(public_router, body, public_api);//intialize public routes
 //------------------------------------------------------------------------------------------------------------------------
+
+
 
 //-------Start Event Loop-------------------------------------------------------------------------------------------------
 //__________________________________________________________________ 
@@ -47,14 +60,15 @@ require('./lib/public_routes.js')(public_router, body, public_api);//intialize p
 //Each of the middleware component are organized into nodes that require the koa "app".
 //They are called nodes, because of what they characterize. Each node represents a set of processes, and may include for than one app.use
 // down flow (node1) => (node2) => ... (node x), upflow (node x) => .. => (node 1).
-// Each Node can continue the flow, Split the flow*, or terminate the downflow. When the downflow if terminate, that the process works back up.
+// Each Node can continue the flow, Split the flow*(planned), or terminate the downflow. When the downflow if terminate, that the process works back up.
 //__________________________________________________________________ 
-
-//Error handler
-nodes.error(app);
+// (logger) => (error) => (limiter) => (session) => (public routes) => (authentication check) => (authenticated routes)
 
 //Logger, write errors to the console
 nodes.logger(app,logger,true);
+
+//Error handler
+nodes.error(app);
 
 //limiter
 nodes.limiter(app,limit);
@@ -70,7 +84,7 @@ nodes.public_router(app,public_router,mount,body,public_api);
 //validate session
 // If validated, move to public routes.
 // If not validated, then return 400 error.
-nodes.validate(app);
+nodes.authenticated(app);
 
 //The following routes require a valid session.
 //For routes see ./lib/routes.js
