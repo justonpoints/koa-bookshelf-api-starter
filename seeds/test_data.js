d27463d493acd8b6b5167384efa606f3cc7e63f1
Juston Points <justonpoints@gmail.com>
2015-07-12 22:56:35 -0700

    added starter seeds

---
+++ seeds/test_data.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('user').del(), 
+
+    // Inserts seed entries
+    knex('user').insert({id:1,name: "one", password: 'password'}),
+    knex('user').insert({id:2,name: "two", password: 'password'}),
+    knex('user').insert({id:3,name: "three", password: 'password'})
+    //look into knex-cleaner
+
+  );
+};
